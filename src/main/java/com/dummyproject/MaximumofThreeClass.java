083dd5e80fd8cf5ff58d4fa46cd73b41330b037e
vasamvishal12@gmail.com <vasamvishal12@gmail.com>
2019-11-28 17:27:44 +0530

    Refactored for three test cases

--- src/main/java/com/dummyproject/MaximumofThreeClass.java
+++ src/main/java/com/dummyproject/MaximumofThreeClass.java
@@ -1,43 +1,9 @@
 package com.dummyproject;
 
 public class MaximumofThreeClass {
-    public Integer threeIntegers() {
-        Integer firstnumber = 10;
-        Integer secondnumber = 20;
-        Integer lastnumber = 30;
 
-        Integer maximum = lastnumber;
-        if (firstnumber.compareTo(maximum) > 0) {
-            maximum = firstnumber;
-        } else if (secondnumber.compareTo(maximum) > 0) {
-            maximum = secondnumber;
-        } else if (lastnumber.compareTo(maximum) > 0) {
-            maximum = lastnumber;
-        }
-        return maximum;
-    }
-
-    public Character threeCharacters() {
-        Character firstnumber = 'a';
-        Character secondnumber = 'b';
-        Character lastnumber = 'c';
-
-        Character maximum = lastnumber;
-        if (firstnumber.compareTo(maximum) > 0) {
-            maximum = firstnumber;
-        } else if (secondnumber.compareTo(maximum) > 0) {
-            maximum = secondnumber;
-        } else if (lastnumber.compareTo(maximum) > 0) {
-            maximum = lastnumber;
-        }
-        return maximum;
-    }
-
-    public Float threeFloat() {
-        Float firstnumber = 1.0f;
-        Float secondnumber = 2.0f;
-        Float lastnumber =3.0f;
-        Float maximum = lastnumber;
+    public <E extends Comparable> E printArray(E firstnumber, E secondnumber, E lastnumber) {
+        E maximum = lastnumber;
         if (firstnumber.compareTo(maximum) > 0) {
             maximum = firstnumber;
         } else if (secondnumber.compareTo(maximum) > 0) {
@@ -48,6 +14,9 @@ public class MaximumofThreeClass {
         return maximum;
     }
 }
+
+
+
 
 
 
--- src/test/java/com/dummyproject/MaximumofThreeTest.java
+++ src/test/java/com/dummyproject/MaximumofThreeTest.java
@@ -7,19 +7,21 @@ public class MaximumofThreeTest {
     @Test
     public void shouldReturn_Maximum_forIntegers() {
         MaximumofThreeClass maximum = new MaximumofThreeClass();
-        Integer integers = maximum.threeIntegers();
-        Assert.assertSame(30, integers);
+        Integer integer = maximum.printArray(10,20,30);
+        Assert.assertSame(30, integer);
     }
+
     @Test
     public void shouldReturn_Maximum_forCharacters() {
         MaximumofThreeClass maximum = new MaximumofThreeClass();
-        Character character = maximum.threeCharacters();
+        Character character = maximum.printArray('a','b','c');
         Assert.assertSame('c',character);
     }
+
     @Test
     public void shouldReturn_Maximum_forFloat() {
         MaximumofThreeClass maximum = new MaximumofThreeClass();
-        Float aFloat = maximum.threeFloat();
+        Float aFloat = maximum.printArray(1.0f,2.0f,3.0f);
         Assert.assertEquals(3.0f,aFloat,0.01);
     }
 }

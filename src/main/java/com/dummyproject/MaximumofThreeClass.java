493eb848c8092d0acaab5619a0118b4b434a3f75
vasamvishal12@gmail.com <vasamvishal12@gmail.com>
2019-11-28 16:30:26 +0530

    Added usercase2 for finding largest character among characters

--- src/main/java/com/dummyproject/MaximumofThreeClass.java
+++ src/main/java/com/dummyproject/MaximumofThreeClass.java
@@ -16,6 +16,22 @@ public class MaximumofThreeClass {
         }
         return maximum;
     }
+
+    public Character threeCharacters() {
+        Character firstnumber = 'a';
+        Character secondnumber = 'b';
+        Character lastnumber = 'c';
+
+        Character maximum = lastnumber;
+        if (firstnumber.compareTo(maximum) > 0) {
+            maximum = firstnumber;
+        } else if (secondnumber.compareTo(maximum) > 0) {
+            maximum = secondnumber;
+        } else if (lastnumber.compareTo(maximum) > 0) {
+            maximum = lastnumber;
+        }
+        return maximum;
+    }
 }
 
 
--- src/test/java/com/dummyproject/MaximumofThreeTest.java
+++ src/test/java/com/dummyproject/MaximumofThreeTest.java
@@ -10,4 +10,10 @@ public class MaximumofThreeTest {
         Integer integers = maximum.threeIntegers();
         Assert.assertSame(30, integers);
     }
+    @Test
+    public void shouldReturn_Maximum_forCharacters() {
+        MaximumofThreeClass maximum = new MaximumofThreeClass();
+        Character character = maximum.threeCharacters();
+        Assert.assertSame('c',character);
+    }
 }

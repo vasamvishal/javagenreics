5a2f1d84ec7179117e37bf865cbeea45d5a40821
vasamvishal12@gmail.com <vasamvishal12@gmail.com>
2019-11-28 10:57:35 +0530

    Generic class 3 program

--- src/main/java/com/dummyproject/Arrayprint.java
+++ src/main/java/com/dummyproject/Arrayprint.java
@@ -1,40 +1,33 @@
 package com.dummyproject;
 
-public class Arrayprint {
-
+public class Arrayprint <X,Y,Z > {
+ X[] myXArray;
+ Y[] myYArray;
+ Z[] myZArray;
+        public Arrayprint(X[] myXArray,Y[] myYArray,Z[] myZArray){
+            this.myXArray=myXArray;
+            this.myYArray=myYArray;
+            this.myZArray=myZArray;
+        }
     public static void main(String args[]){
         Integer array[]={1,2,3};
         Double darray[]={1.0,2.0,3.0};
         Character carray[]={'a','b','c'};
-        toPrint(array);
-        toPrint(darray);
-        toPrint(carray);
 
-
+        new Arrayprint<Integer,Double,Character>(array,darray,carray).toPrint();
     }
 
+    public  void toPrint() {
+            toPrint(myXArray);
+            toPrint(myYArray);
+            toPrint(myZArray);
+    }
     private static <E> void toPrint(E[] array) {
         for(E i: array){
             System.out.println(i);
         }
     }
 
-    private static void toPrint(char[] carray) {
-        for (int i = 0; i < carray.length; i++) {
-            System.out.println(carray[i]);
-        }
-    }
 
-    private static void toPrint(double[] darray) {
-        for (int i = 0; i < darray.length; i++) {
-            System.out.println(darray[i]);
-        }
 
-    }
-
-    private static void toPrint(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-        }
-    }
 }
